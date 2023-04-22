b606c787eee5f7f2f1105efc7673a3688cc3a9b8
diesel64 <supershanio64@gmail.com>
2023-04-22 01:18:59 -0400

    Update chrmodels.h

--- Dreamcast Blaze/chrmodels.h
+++ Dreamcast Blaze/chrmodels.h
@@ -1,3 +1,9 @@
 #pragma once
 
 extern void __cdecl WriteBlazeModel();
+
+// texture scroll
+NJS_TEX uv_8DB372077395BE39B46[];
+NJS_TEX uv_8DB372074B70C15AB6D[];
+NJS_TEX uv_8DB37207C2178CC5D96[];
+NJS_TEX uv_8DB372079D61F441885[];
