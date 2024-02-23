4d8701ef789e269ba6ebde803950debf202c107a
diesel64 <supershanio64@gmail.com>
2024-02-23 11:33:44 -0500

    Update chrmodels.h
    Glowing eyes

--- Dreamcast Blaze/chrmodels.h
+++ Dreamcast Blaze/chrmodels.h
@@ -3,7 +3,13 @@
 extern void __cdecl WriteBlazeModel();
 
 // texture scroll
-NJS_TEX uv_8DB372077395BE39B46[];
-NJS_TEX uv_8DB372074B70C15AB6D[];
-NJS_TEX uv_8DB37207C2178CC5D96[];
-NJS_TEX uv_8DB372079D61F441885[];
+extern NJS_TEX uv_8DB372077395BE39B46[];
+extern NJS_TEX uv_8DB372074B70C15AB6D[];
+extern NJS_TEX uv_8DB37207C2178CC5D96[];
+extern NJS_TEX uv_8DB372079D61F441885[];
+
+// glowing eyes in dark areas
+extern NJS_OBJECT object_005698F0;
+extern NJS_OBJECT object_00569CE8;
+extern NJS_OBJECT object_0062C828;
+extern NJS_OBJECT object_0062CC20;
