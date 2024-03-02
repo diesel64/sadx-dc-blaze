b34cd86d08ccbcd5c0b2c3825ceefb00a52e64cc
diesel64 <supershanio64@gmail.com>
2024-03-01 20:04:55 -0500

    Update hover.h
    Same as before

--- Dreamcast Blaze/hover.h
+++ Dreamcast Blaze/hover.h
@@ -1,6 +1,9 @@
 #pragma once
 
+extern Buttons hoverButton;
 extern char hoverTimerGlobal;
 extern bool hoverUsedGlobal;
 void hover_Physics(taskwk* data, motionwk2* data2, playerwk* co2);
 signed int hover_CheckInput(taskwk* data, playerwk* co2);
+
+#define TWP_PNUM(twp) twp->counter.b[0]
